5bb59372ad5a5667ebc70e117cf1912479f1db04
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-04-14 15:10:28 -0700

    Added data state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const data = {
+  // Temporary initial data
+  const initialData = {
     hits: [
       { objectID: 1, title: "Google", url: "http://google.ca" },
     ]
   };
+
+  // Keep track of "data" as State. Initialize with some initial Data
+  const [data, setData] = useState(initialData);
+  // Note the Javascript Destructuring.  Same result as this:
+  // const myState = useState(initialData);
+  // const data = myState[0];     // state item
+  // const setData = myState[1]; // updating function (setter)
 
   // Build list of 'hits'
   const hitsList = data.hits.map(item => (
@@ -18,6 +26,7 @@ function App() {
 
   return (
     <div className="App">
+      <h3>My Search Page</h3>
       <ul>{hitsList}</ul>
     </div>
   );
