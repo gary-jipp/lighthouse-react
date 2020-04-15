5b7bc89281231e72e5d95304a5e81e60c559d4ee
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-04-14 18:18:58 -0700

    Remove embedded control

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
 
   // Application State
   const [data, setData] = useState({ hits: [] });
-  const [query, setQuery] = useState("react");
   const [url, setUrl] = useState(); // Won't need this anymore
 
   // Fetch data whenever url changes
@@ -27,21 +26,11 @@ function App() {
     <div className="App">
       <h3>My Search Page</h3>
 
-      <h4>{query}</h4>
       <h4>{url}</h4>
 
       {/* Use a component for the Search Control*/}
       <Search initial="React"
         onClick={(query) => setUrl(BASEURL + query)} />
-
-      {/* Won't need this anymore */}
-      <input type="text" value={query}
-        onChange={event => setQuery(event.target.value)} />
-      <button
-        type="button"
-        onClick={() => setUrl(BASEURL + query)}>
-        Search
-			</button>
 
       <ul>{hitsList}</ul>
     </div>
