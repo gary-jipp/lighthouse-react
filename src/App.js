072a082d6fbcb9260e3854c189de0788da1482c5
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-09-02 23:28:15 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 
-function App() {
+// Initial data
+const initialData = [
+  { objectID: 1, title: "Google", url: "http://google.ca" },
+];
 
-  // Temporary initial data
-  const initialData = {
-    hits: [
-      { objectID: 1, title: "Google", url: "http://google.ca" },
-    ]
-  };
+export default function App() {
 
   // Keep track of "data" as State. Initialize with some initial Data
   const [data, setData] = useState(initialData);
-  // Note the Javascript Destructuring.  Same result as this:
-  // const myState = useState(initialData);
-  // const data = myState[0];     // state item
-  // const setData = myState[1]; // updating function (setter)
 
   // Keep track of " query" 
   const [query, setQuery] = useState("react");
 
   // Build (array) of <li> elements from  data.hits
-  const hitsList = data.hits.map(item => (
+  const hitsList = data.map(item => (
     <li key={item.objectID}>
       <a href={item.url}>{item.title}</a>
     </li>
@@ -33,15 +27,13 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <Fragment className="App">
       <h3>My Search Page</h3>
 
       <h4>{query}</h4>
       <input type="text" value={query} onChange={onChange} />
 
       <ul>{hitsList}</ul>
-    </div>
+    </Fragment>
   );
 }
-
-export default App;
