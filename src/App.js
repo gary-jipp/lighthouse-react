959aedb1565140b85a8f2c5d0c259cdbe0c7c401
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-09-02 23:03:26 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+
+const data = {
+  hits: [
+    { objectID: 1, title: "Google", url: "http://google.ca" },
+    { objectID: 2, title: "Amazon", url: "http://amazon.ca" },
+  ]
+};
 
 function App() {
 
-  const data = {
-    hits: [
-      { objectID: 1, title: "Google", url: "http://google.ca" },
-      { objectID: 2, title: "Amazon", url: "http://amazon.ca" },
-    ]
-  };
+  // React will just render whatever you give it
+  const header = <h3>This is a Header</h3>;
+  const div = <div>This is stuff inside a div</div>;
+  const li = <li>This is a list item</li>;
 
-  const list = [<li>This is a list item</li>];
+  const array =
+    [
+      <li>This is a list item in an array</li>,
+      <li>This is a list item in an array</li>
+    ];
 
   // Build (array) of <li> elements from  data.hits
   const hitsList = data.hits.map(item =>
@@ -21,7 +30,12 @@ function App() {
 
   return (
     <div className="App">
-      <ul>{list}</ul>
+      {header}
+      {div}
+      <ul>
+        {li}
+        {array}
+      </ul>
     </div>
   );
 }
